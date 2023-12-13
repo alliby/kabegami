42183fe130ba0e4c90fd4f296545592a6b89131e
alliby <mouhamir2@gmail.com>
2023-12-13 20:28:41 +0100

    Add version cli argument

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ mod linux;
 #[cfg(target_os = "linux")]
 use linux::LinuxSetter as PlatformSetter;
 
+const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn modes_str_fn(_value: &str) -> Result<PaperMode, String> {
     match _value {
         "strim" => Ok(PaperMode::Strim),
@@ -23,11 +25,15 @@ fn modes_str_fn(_value: &str) -> Result<PaperMode, String> {
 pub struct Cli {
     /// the path to the images directory or image file
     #[argh(positional, arg_name = "PATH")]
-    pub path: PathBuf,
+    pub path: Option<PathBuf>,
 
     /// default mode: strim, available modes: strim, stretch, fill
     #[argh(option, default = "PaperMode::default()", from_str_fn(modes_str_fn))]
     pub mode: PaperMode,
+
+    /// print version info
+    #[argh(switch, short = 'V')]
+    pub version: bool,
 }
 
 fn read_dir(path: PathBuf) -> error::Result<impl Iterator<Item = PathBuf>> {
@@ -39,9 +45,16 @@ fn read_dir(path: PathBuf) -> error::Result<impl Iterator<Item = PathBuf>> {
 
 fn run() -> error::Result<()> {
     let cli: Cli = argh::from_env();
-    let path = cli.path;
+    if cli.version {
+        println!("kabegami v{PKG_VERSION}");
+        std::process::exit(0);
+    }
+    let Some(path) = cli.path else {
+        eprintln!("no path was provided");
+        std::process::exit(1);
+    };
     let mode = cli.mode;
-
+    
     if path.is_dir() {
         let dir_paths = read_dir(path)?;
         PlatformSetter::set_random_wallpaper(dir_paths, mode)
